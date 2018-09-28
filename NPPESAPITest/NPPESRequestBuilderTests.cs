b4202dec796dcdb4798460ecb7a47c21af7a2c96
Daniel Destouche <daniel.destouche@gmail.com>
2018-09-28 12:01:48 -0400

    Update NPPESApiClient with a shorter method to request NPI Numbers, update unit tests

---
+++ NPPESAPITest/NPPESRequestBuilderTests.cs
@@ -0,0 +1,28 @@
+﻿using Forcura.NPPES;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NPPESAPITest
+{
+    [TestClass]
+    public class NPPESRequestBuilderTests
+    {
+        [TestMethod]
+        public void NPPESRequestBuilder_UpdatesValue()
+        {
+            // arrange
+            var builder = new NPPESRequestBuilder();
+            const string firstNumber = "1234567890";
+            const string lastNumber = "0987654321";
+
+            // act
+            var request = builder
+                .Number(firstNumber)
+                .Number(lastNumber)
+                .Build();
+
+            // assert
+            Assert.IsNotNull(request);
+            Assert.AreEqual(lastNumber, request.Number);
+        }
+    }
+}
